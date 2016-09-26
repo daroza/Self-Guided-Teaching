e1f67e7c65bfaba0fecc604b0751dd2be08a5a14
l4nk332 <l4nk332@gmail.com>
2016-09-26 09:21:36 -0700

    Completed todays warmup

--- Unit-01/Warm-ups/09_26_16.js
+++ Unit-01/Warm-ups/09_26_16.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
 The goal of this exercise is to convert a string to a new string where each character in the new string is '(' if that character appears only once in the original string, or ')' if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
 
@@ -11,3 +12,32 @@ Examples:
 
 "(( @" => "))(("
 */
+
+// 1. Normalize string
+// 2. Map each letter to a numeric value
+// 3. Increment the letter value in the map when duplicate is reached.
+// 4. Reduce the keys of the object to string based on values.
+
+function replaceDuplicates(str) {
+	let normalized = str.toLowerCase();
+	let charMap = {};
+
+	for (let idx = 0; idx < normalized.length; idx++) {
+		let currentChar = normalized[idx];
+		if (!charMap.hasOwnProperty(currentChar)) {
+			charMap[currentChar] = 1;
+		} else {
+			charMap[currentChar]++;
+		}
+	}
+
+	let converted = normalized.split("").reduce((acc, ch) => {
+	    return acc + ((charMap[ch] > 1) ? ")" : "(");
+	}, "");
+
+	return converted;
+}
+
+let input = process.argv[2] || "recede";
+
+console.log(`${input} => ${replaceDuplicates(input)}`);
