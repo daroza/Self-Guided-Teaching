a7170f078849fec912668c3fe5060c0ee97524d4
l4nk332 <l4nk332@gmail.com>
2016-09-27 10:18:40 -0700

    Passed today's warmup

--- Unit-01/Warm-ups/09_27_16.py
+++ Unit-01/Warm-ups/09_27_16.py
@@ -7,3 +7,21 @@
 # longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
 #
 # n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
+
+def longest_consec(strarr, k):
+    longest = ""
+    if k > len(strarr) or k <= 0 or len(strarr) == 0:
+        return longest
+    idx_a = 0
+    idx_b = k
+
+    while idx_b < len(strarr)+1:
+        current_slice = "".join(strarr[idx_a : idx_b])
+        if len(longest) < len(current_slice):
+            longest = current_slice
+        idx_a += 1
+        idx_b += 1
+
+    return longest
+
+print(longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2))
